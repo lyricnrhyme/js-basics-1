9a27a8bab996859e65ba1af17e9cce14ccf56547
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-07 19:52:09 -1000

    Created name variable, person object, canDrive boolean, greet function

--- basics.js
+++ basics.js
@@ -1,18 +1,30 @@
 /* Create a `myName` variable and assign it a String value */
+var myName = "Jamie";
 
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
+var person = {
+    name: "Jamie",
+    age: 24
+}
 
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+if (person.age >= 16) {
+    var canDrive = true;
+}
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
+function greet(name) {
+    console.log("Hello, my name is " + name);
+}
+greet(myName);
 
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
